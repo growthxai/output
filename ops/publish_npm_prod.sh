1c597dc86eb900d0db96307bda9602c1a5b86f1b
Stéfano Zanata <221676+szanata@users.noreply.github.com>
2026-03-17 15:49:42 +0000

    chore(ops): New npm publish scripts (#19)
    * chore: Adding new simpler script to publish pkgs to NPM
    
    * chore: Adding new version of the dev package publishing script
    
    * removing extraneous file
    
    * chore: Updating relative path of publish_npm_prod command

--- ops/publish_npm_prod.sh
+++ ops/publish_npm_prod.sh
@@ -1,59 +1,19 @@
 #!/bin/bash
 
-# Publish prod versions of the SDK packages to NPM.
-# For each package in the sdk it will check if the package version is different from NPM.
-# If it is, the script will publish the new version.
+# Publish each package to NPM (if version is not published)
 
 set -e
 
-cd "${0%/*}"
+cd "${0%/*}/.."
 
 count=0
 print() {
-  local sym=${2:-$((++count))}
-  bg=$([[ $sym == "┗" ]] && echo "2" || echo "45" )
-  fg=$([[ $sym == "┗" ]] && echo "35" || echo "30" )
-  printf "\e[$bg;"$fg"m %s \e[0m %b\e[0m\n" "$sym" "$1"
+  sym=${2:-$((++count))}
+  printf "\e[45;30m $sym \e[0m $1\n"
 }
 
-print "Publishing SDK" "Run"
+print "Publishing packages to NPM" "Run"
 
-deploy() {
-  path=$1
-
-  print "Publishing $path"
-
-  name=$(node -p "require('$path/package.json').name")
-  local_version=$(node -p "require('$path/package.json').version")
-  remote_version=$(pnpm view "$name@$local_version" version 2>/dev/null || echo "unpublished")
-
-  if [[ $local_version == $remote_version ]]; then
-    print "Ignoring $name, version $local_version is already published" "┗"
-  else
-    print "Publishing $name@$local_version" "┗"
-
-    if ! output=$(cd "$path" && pnpm publish --no-git-checks 2>&1); then
-      print "\e[0;31mFail:\n↓ ↓ ↓\n$(printf '%s\n' "$output" | grep -E '^ERR_PNPM' || true)\n↑ ↑ ↑" "┗"
-      exit 1
-    else
-      print "\e[0;32mPublished" "┗"
-    fi
-
-  fi
-}
-
-# Dynamically find all SDK packages, ensuring framework is published last
-PACKAGES=()
-for dir in ../sdk/*/; do
-  if [[ -d "$dir" && -f "$dir/package.json" && "$dir" != *"/framework/" ]]; then
-    PACKAGES+=("$dir")
-  fi
-done
-# Framework must be published last as it depends on the others
-PACKAGES+=("../sdk/framework")
-
-for path in "${PACKAGES[@]}"; do
-  deploy "$path"
-done
+pnpm publish -r --no-git-checks
 
 print "Publication Complete" "OK"
