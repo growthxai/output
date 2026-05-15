58ff8f9ea0520aa9a187ebc25f5f477f1f986c86
Stéfano Zanata <221676+szanata@users.noreply.github.com>
2026-05-15 23:49:13 +0100

    Improving aggregation math calculations

--- sdk/core/src/interface/aggregations.js
+++ sdk/core/src/interface/aggregations.js
@@ -5,16 +5,17 @@ export const aggregateAttributes = attributes => ( {
   cost: {
     total: attributes
       .filter( a => [ Attribute.HTTPRequestCost.TYPE, Attribute.LLMUsage.TYPE ].includes( a.type ) )
-      .reduce( ( sum, a ) => Decimal( a.total ).add( sum ).toNumber(), 0 )
+      .reduce( ( sum, a ) => sum.add( a.total ), Decimal( 0 ) ).toNumber()
   },
   tokens: {
     total: attributes
       .filter( a => Attribute.LLMUsage.TYPE === a.type )
-      .reduce( ( sum, a ) => Decimal( a.tokensUsed ).add( sum ).toNumber(), 0 ),
-    ...attributes
+      .reduce( ( sum, a ) => sum.add( a.tokensUsed ), Decimal( 0 ) ).toNumber(),
+    ...Object.entries( attributes
       .filter( a => Attribute.LLMUsage.TYPE === a.type )
       .flatMap( a => a.usage )
-      .reduce( ( obj, a ) => Object.assign( obj, { [a.type]: Decimal( obj[a.type] ?? 0 ).add( a.amount ).toNumber() } ), {} )
+      .reduce( ( obj, a ) => Object.assign( obj, { [a.type]: ( obj[a.type] ?? Decimal( 0 ) ).add( a.amount ) } ), {} ) )
+      .reduce( ( obj, [ k, v ] ) => Object.assign( obj, { [k]: v.toNumber() } ), {} ) // convert all values to number
 
   },
   httpRequests: {
