05736edc022c26ffd84bf8e41835cb14479808bc
Stéfano Zanata <221676+szanata@users.noreply.github.com>
2026-06-18 21:09:29 +0100

    Updating messages

--- sdk/core/src/worker/index.js
+++ sdk/core/src/worker/index.js
@@ -107,16 +107,22 @@ const execute = async () => {
    * Temporal worker shutdown is a bit odd.
    * worker.run() is an async job that only resolves when calling worker.shutdown().
    * But worker.shutdown() is not async and returns nothing, so there is no way to await it.
-   * All code that needs to run after shutdown needs to be after await worker.run().
+   * All code that needs to run after shutdown needs to be after `await worker.run()`.
    *
    * The following code needs to cover these scenarios:
-   * 1. Connection monitor detects connection loss and stop the worker gracefully
-   * 2. Catalog.run() has a failure and stop the worker and connection monitor gracefully
-   * 2. Interruption is received and stop the connection monitor and worker gracefully
-   * 3. Worker throws an error
+   * 1. Connection monitor detects connection loss
+   * 2. Catalog.run() has a failure
+   * 3. Interruption is received
+   * 4. Worker throws an error
+   *
+   * For each scenario all promises in the Promise.all() need to be completed via functions:
+   * connectionMonitor.stop(), catalogJob.interrupt(), worker.shutdown()
    */
 
-  /** Graceful shutdown */
+  /**
+   * Graceful shutdown
+   * Triggers the actions that will resolve all promises in the Promise.all(), so the code can resume
+   */
   const shutdown = runOnce( () => {
     log.info( 'Shutdown started...' );
     if ( worker.getStatus().runState === 'RUNNING' ) {
@@ -126,7 +132,7 @@ const execute = async () => {
     state.catalogJob.interrupt();
   } );
 
-  /** When receiving an interruption, stop both promises that are long lived: worker and connectionMonitor */
+  /** When receiving an interruption, call shutdown */
   setupInterruptionHandler( shutdown );
 
   /** When the connection is lost, call shutdown */
@@ -135,9 +141,15 @@ const execute = async () => {
   /** If the catalog job manager fails, call shutdown */
   state.catalogJob.onError( shutdown );
 
-  /** Runs the worker, connection monitor (long lived) and catalogJob (ephemeral) */
+  /**
+   * Runs the worker, connection monitor and catalogJob (ephemeral)
+   * None of these will reject in normal conditions. Errors need to be inspected later
+   * They will resolve only when calling the actions in shutdown(),
+   * except catalogJob, which can resolve by itself given a bit of time
+   */
   log.info( 'Running worker...' );
   await Promise.all( [
+    // When the worker fails, store the error and call shutdown
     worker.run().catch( error => {
       state.workerError = error;
       shutdown();
@@ -148,12 +160,13 @@ const execute = async () => {
 
   log.info( 'Worker terminated' );
 
+  /** After the Promise.all() is resolved, check which services had an error, since none rejects the promise */
   const error =
     state.connectionMonitor.connectionLossError ??
     state.catalogJob.error ??
     state.workerError;
 
-  /** After the shutdown, check for errors, if any found, throw it */
+  /** If any error is found, throws it, so this process can exit with code=1 (failure) */
   if ( error ) {
     throw error;
   }
@@ -161,7 +174,14 @@ const execute = async () => {
 
 execute()
   .finally( async () => {
-    // All errors here are ignored to not mask actual errors, and because this is already a shutdown
+    /**
+     * This will make sure that if we had any uncaught failures, everything is tore down.
+     * Ignore any errors here in order to not mask actually errors from before and because at this point
+     * the code is already shutting down, so no need to throw anyway.
+     *
+     * worker.shutdown() is not tried here, because it cannot be awaited. By this point is safe to say
+     * that worker never started, already crashed or was stopped anyway.
+     */
     if ( state.connectionMonitor?.running ) {
       log.info( 'Stopping connection monitor...' );
       await state.connectionMonitor.stop()
