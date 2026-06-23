897438f48d042d35cd78502190f44414649bb980
Stéfano Zanata <221676+szanata@users.noreply.github.com>
2026-06-23 22:21:41 +0100

    Adding more comments

--- sdk/core/src/interface/logger.js
+++ sdk/core/src/interface/logger.js
@@ -3,14 +3,18 @@ import { proxySinks } from '@temporalio/workflow';
 import { validateLogArguments } from './validations/index.js';
 import { ACTIVITY_LOGGER_SYMBOL } from '#consts';
 
+// This is inoffensive and can be used outside workflow sandbox
 const sinks = proxySinks();
 
 const log = ( level, message, metadata ) => {
   validateLogArguments( { message, metadata } );
+  // When inside workflow, use sinks to send logs out
   if ( inWorkflowContext() ) {
     sinks.workflow.log( { level, message, metadata } );
+  // When inside activities, use the global function to ship logs
   } else if ( typeof globalThis[ACTIVITY_LOGGER_SYMBOL] === 'function' ) {
     globalThis[ACTIVITY_LOGGER_SYMBOL]( { level, message, metadata } );
+  // This fallback is used on unit tests
   } else {
     console.log( `logger.${level}`, message, metadata );
   }
