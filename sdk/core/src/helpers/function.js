c170b244edb8eed45e98b05a3af44a2ddb1fca30
Stéfano Zanata <221676+szanata@users.noreply.github.com>
2026-09-08 14:04:43 +0100

    Dropped unnused helpers

--- sdk/core/src/helpers/function.js
+++ sdk/core/src/helpers/function.js
@@ -1,20 +1,4 @@
 /**
- * Returns a function that invokes the fn argument only once when called, further calls do nothing.
- * @param {Function} fn
- * @returns {Function}
- */
-export const runOnce = fn => {
-  const state = { executed: false, result: undefined };
-  return ( ...args ) => {
-    if ( !state.executed ) {
-      state.executed = true;
-      return state.result = fn( ...args );
-    }
-    return state.result;
-  };
-};
-
-/**
  * Calls a given function and in case it throws errors, returns `undefined`
  * @param {Function} fn
  * @returns {unknown|undefined}
