8ff8cbc24ee8266058720d323514b0850f585197
Stéfano Zanata <221676+szanata@users.noreply.github.com>
2026-09-09 17:14:02 +0100

    Fixed typo

--- sdk/core/src/worker/shutdown.js
+++ sdk/core/src/worker/shutdown.js
@@ -5,13 +5,13 @@ const log = createChildLogger( 'Worker Shutdown' );
 
 /* Gracefully shutdown all services, ignore errors */
 export const shutdownServices = async ( { workerRunner, connectionMonitor, catalogPublisher, connection } ) => {
-  const shudownJobs = [
+  const shutdownJobs = [
     { label: 'Stopping Worker', isActive: () => workerRunner?.running, stop: () => workerRunner.stop() },
     { label: 'Stopping Connection Monitor', isActive: () => connectionMonitor?.running, stop: () => connectionMonitor.stop() },
     { label: 'Interrupting Catalog Publisher', isActive: () => catalogPublisher?.running, stop: () => catalogPublisher.interrupt() },
     { label: 'Closing Connection', isActive: () => connection, stop: () => connection.close() }
   ];
-  for ( const { label, isActive, stop } of shudownJobs ) {
+  for ( const { label, isActive, stop } of shutdownJobs ) {
     if ( isActive() ) {
       log.info( `${label}...` );
       await stop().catch( e => log.warn( `${label} error`, { error: serializeError( e ) } ) );
